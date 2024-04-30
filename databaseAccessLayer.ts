c999e859e7bd8beddfdc903b1be00422e40ab651
[Jin] <hahn23@my.bcit.ca>
2024-04-30 12:29:02 -0700

    DB connection

---
+++ databaseAccessLayer.ts
@@ -0,0 +1,9 @@
+import database from './databaseConnection';
+
+// 필요한 기능이나 객체를 export 할 수 있습니다.
+export const someFunction = () => {
+  // 함수 로직
+};
+
+// 현재 예제에서는 빈 객체를 내보내고 있으므로, 필요한 기능을 추가하거나 내보낼 객체를 구성해야 합니다.
+export {};
