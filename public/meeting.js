529a3f513bcf58c49189f45ed53b91dfe77637cf
[Jin] <hahn23@my.bcit.ca>
2024-05-02 18:03:10 -0700

    showing user video on browser

--- public/meeting.js
+++ public/meeting.js
@@ -15,6 +15,71 @@ socket.addEventListener("close", () => {
 
 const socket = io();
 
+const myFace = document.getElementById("myFace");
+const muteBtn =  document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+const getCamera = async () => {
+  try {
+    const device = await navigator.mediaDevices.enumerateDevices();
+    const cameras = device.filter((device) => device.kind === "videoinput");
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      camerasSelect.appendChild(option);
+    });
+  } catch(e) {
+    console.log(e);
+  }
+}
+getCamera();
+const getMedia = async () => {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    myFace.srcObject = myStream;
+  } catch(e) {
+    console.log(e);
+  }
+}
+getMedia();
+const handleMuteClick = () => {
+  myStream.getAudioTracks().forEach((track) => {
+    track.enabled = !track.enabled;
+  });
+ if(!muted) {
+  muteBtn.innerText = "Unmute";
+  muted = true;
+ } else {
+  muteBtn.innerText = "Mute";
+  muted = false;
+ }
+}
+
+const handleCameraClick = () => {
+  myStream.getVideoTracks().forEach((track) => {
+    track.enabled = !track.enabled;
+  }); 
+  if(cameraOff) {
+    cameraBtn.innerText = "Turn Camera Off";
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = "Turn Camera On";
+    cameraOff = true;
+  }
+}
+
+muteBtn.addEventListener("click", handleMuteClick)
+cameraBtn.addEventListener("click", handleCameraClick)
+
+
 const welcome = document.getElementById("welcome");
 const form = welcome.querySelector("form");
 const room = document.getElementById("room");
@@ -23,11 +88,15 @@ room.hidden = true;
 
 let roomName;
 
+
+
 const handleMessageSubmit = (event) => {
   event.preventDefault();
   const input = room.querySelector("input");
+  const value = input.value;
   socket.emit("new_message", input.value,roomName, () => {
-    addMessage(`You: ${input.value}`, input);
+    addMessage(`You: ${value}`, input);
+  input.value = "";
   });
   
 }
@@ -37,8 +106,9 @@ function showRoom() {
   room.hidden = false;
   const h3 = room.querySelector("h3");
   h3.innerHTML = `Room: ${roomName}`;
-  const form = room.querySelector("form");
-  form.addEventListener("submit", handleMessageSubmit);
+  const msgForm = room.querySelector("#msg");
+  const nameForm = room.querySelector("#name") ;
+  msgForm.addEventListener("submit", handleMessageSubmit);
 
 }
 
